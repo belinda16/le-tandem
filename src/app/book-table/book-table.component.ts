743623f6441db543b7aff333e41963d0f26d6cc7
belinda16 <favorbelinda@gmail.com>
2024-08-08 17:15:49 +0000

    ...

--- src/app/book-table/book-table.component.ts
+++ src/app/book-table/book-table.component.ts
@@ -1,12 +1,40 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-book-table',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './book-table.component.html',
   styleUrl: './book-table.component.scss'
 })
 export class BookTableComponent {
+
+  images = ["assets/images/hotel-gallery-one.png", "assets/images/hotel-gallery-two.png", "assets/images/hotel-gallery-three.png"];
+
+  currentIndex = 0;
+
+  get prevIndex() {
+    return (this.currentIndex + this.images.length - 1) % this.images.length;
+  }
+
+  get nextIndex() {
+    return (this.currentIndex + 1) % this.images.length;
+  }
+
+  prevImage() {
+    this.currentIndex = this.prevIndex;
+    this.scrollImages();
+  }
+
+  nextImage() {
+    this.currentIndex = this.nextIndex;
+    this.scrollImages();
+  }
+
+  scrollImages() {
+    // Logic to smoothly transition images
+  }
+
 
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { NgbDatepicker, NgbDatepickerModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgbDatepicker, NgbDatepickerModule, NgbDateStruct } from '@ng-bootstrap/ng-bootstrap';
 import { FlexLayoutModule } from 'ngx-flexible-layout';
 import { SlickCarouselModule } from 'ngx-slick-carousel';
+import { FooterComponent } from "../footer/footer.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [FlexLayoutModule, FormsModule, NgbDatepickerModule, SlickCarouselModule],
+  imports: [FlexLayoutModule, FormsModule, NgbDatepickerModule, SlickCarouselModule, FooterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
@@ -16,6 +17,11 @@ export class HomeComponent {
 
   date!: any
 
+
+  checkinDate!: any
+  checkoutDate!: any
+  private _model!: NgbDateStruct;
+
   slides = [
     { img: "assets/images/gallery-two.png" },
     { img: "assets/images/gallery-one.png" },
@@ -23,10 +29,46 @@ export class HomeComponent {
     { img: "assets/images/gallery-three.png" },
     { img: "assets/images/gallery-one.png" },
   ];
+
+
+
+
   slideConfig = {
     "slidesToShow": 4, "slidesToScroll": 1,
     autoplay: true,
     autoplaySpeed: 2000,
   };
+
+
+  images = ["assets/images/hotel-gallery-one.png", "assets/images/hotel-gallery-two.png", "assets/images/hotel-gallery-three.png"];
+
+  currentIndex = 0;
+
+  get prevIndex() {
+    return (this.currentIndex + this.images.length - 1) % this.images.length;
+  }
+
+  get nextIndex() {
+    return (this.currentIndex + 1) % this.images.length;
+  }
+
+  prevImage() {
+    this.currentIndex = this.prevIndex;
+    this.scrollImages();
+  }
+
+  nextImage() {
+    this.currentIndex = this.nextIndex;
+    this.scrollImages();
+  }
+
+  scrollImages() {
+    // Logic to smoothly transition images
+  }
+
+
+
+
+
 
 }
